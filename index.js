fa4e8149940fc184772bb5a17cd60b8c6babc8db
gsambou <ghislain.sambou@gmail.com>
2021-07-27 00:09:09 +0200

    colors

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 #!/usr/bin/env node
-const welcome = require('cli-welcome')
+const welcome = require('cli-welcome');
+const chalk = require('chalk');
 const pkgJSON = require('./package.json');
+const log = console.log;
+
+const color = {
+    twitter: chalk.hex(`#1da1f2`).bold.inverse,
+    github: chalk.hex(`#6cc644`).bold.inverse,
+    blog: chalk.hex(`#6937FF`).bold.inverse,
+    dim: chalk.dim,
+};
+const italic = chalk.italic;
+
 
 welcome({
-    title: pkgJSON.name,
+    title: `MyName SAMBOU`,
     tagLine: `Hello nice to meet u`,
     description: pkgJSON.description,
     version: pkgJSON.version,
-    bgColor: `#FADC00`,
+    bgColor: `#6937FF`,
     color: `#000000`,
     bold: true,
     clear: true,
-})
+});
 
-console.log(`gsambou
-Data Protection Officer - Chargé Protection des données
-- Twitter: https://twitter.com/g_sambou
-- GitHub: https://github.com/gsambou
-- Blog: https://gsambou.com
+log(`
+${italic(`
+Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+`)}
+
+${color.twitter(` Twitter `)}: ${color.dim(`https://twitter.com/g_sambou`)}
+${color.github(` GitHub `)}: ${color.dim(`https://github.com/gsambou`)}
+${color.blog(` Blog `)}: ${color.dim(`https://gsambou.com`)}
 `);
