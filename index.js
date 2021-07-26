ebbd75b47341630f85b80b0ed5e1f898e1fed945
gsambou <ghislain.sambou@gmail.com>
2021-07-26 23:08:31 +0200

    welcome msg

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 #!/usr/bin/env node
-const clearConsole = require('clear-any-console');
+const welcome = require('cli-welcome')
+const pkgJSON = require('./package.json');
 
-clearConsole();
+welcome({
+    title: pkgJSON.name,
+    tagLine: `Hello nice to meet u`,
+    description: pkgJSON.description,
+    version: pkgJSON.version,
+    bgColor: `#FADC00`,
+    color: `#000000`,
+    bold: true,
+    clear: true,
+})
 
 console.log(`gsambou
 Data Protection Officer - Chargé Protection des données
