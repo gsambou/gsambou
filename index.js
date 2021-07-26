47a0664f07a08ffa64fde9a594a58f8971226224
gsambou <ghislain.sambou@gmail.com>
2021-07-26 22:28:34 +0200

    First version

---
+++ index.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+console.log(`gsambou
+Data Protection Officer - Chargé Protection des données
+- Twitter: https://twitter.com/g_sambou
+- GitHub: https://github.com/gsambou
+- Blog: https://gsambou.com
+`);
