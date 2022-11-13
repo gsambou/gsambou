1829176e7c810a8f6fb4bbbc0c629f76590faa27
gsambou <ghislain.sambou@gmail.com>
2022-11-13 20:25:15 +0100

    format lorem text

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 console.log(`
 gsambou
+
 Lorem Ipsum is simply dummy text of the printing and typesetting industry.
 Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
 , when an unknown printer took a galley of type and scrambled it to make a type specimen book.
@@ -9,4 +10,5 @@ Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
 🐣 Twitter:     https://twitter.com/gsambou.dev
 📖 GitHub:      https://github.com/gsambou
 🥷🏽  Blog:        https://gsambou.dev
+
 `);
