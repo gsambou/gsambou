36c3dca0394b64f2d0ef9ab5f36270844e2841e9
gsambou <ghislain.sambou@gmail.com>
2022-11-13 20:19:11 +0100

    format text

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 
 console.log(`
 gsambou
-Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
 
 🐣 Twitter:     https://twitter.com/gsambou.dev
 📖 GitHub:      https://github.com/gsambou
