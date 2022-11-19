2d475a3ee962da6239bb84a10ac535e87e7f2525
gsambou <ghislain.sambou@gmail.com>
2022-11-19 21:55:44 +0100

    remove some files

--- utils/debug.js
+++
@@ -1,8 +0,0 @@
-import alert from 'alert-log';
-
-export default (flag, input) => {
-	alert('warning', 'CLI DATA ⇣ 🔗', 'debugger');
-	console.log(`input`, input);
-	console.log(`flag`, { ...flag });
-	console.log();
-};
